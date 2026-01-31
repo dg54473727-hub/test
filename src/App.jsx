b63c48593cbd1b3c85863ccc8f4763f84ab671f9
admin <dg54473727@gmail.com>
2026-01-31 19:19:50 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,77 @@
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Header from "./components/header";
+import "./index.css";
+import Home from "./pages/home";
+import ArtistPage from "./pages/artist";
+import SongPage from "./pages/song";
+import NotFound from "./pages/notfound";
+import SongInfoPage from "./pages/songPage";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/song" element={<SongPage />} />
+        <Route path="/artist" element={<ArtistPage />} />
+        <Route path="*" element={<NotFound />} />
+        <Route
+          path="/songs/kuuninaru"
+          element={
+            <SongInfoPage
+              img="https://i.ytimg.com/vi/f6TytcA47rI/hq720.jpg"
+              name="くうになる"
+              vocal="初音ミク / 可不"
+              eng="Becoming Empty"
+              lyric="MIMI"
+              aragement="MIMI"
+            />
+          }
+        />
+        <Route
+          path="/songs/helohowareyou"
+          element={
+            <SongInfoPage
+              img="https://i.ytimg.com/vi/-M4sAQYi6r4/hqdefault.jpg"
+              name="くうになる"
+              vocal="初音ミク"
+              eng="Hello/How are you"
+              lyric="Nanou"
+              aragement="Nanou"
+            />
+          }
+        />
+        <Route
+          path="/songs/aiturazenindoushoukai"
+          element={
+            <SongInfoPage
+              img="https://i.ytimg.com/vi/ZUwaudw8ht0/hq720.jpg"
+              name="あいつら全員同窓会"
+              vocal="ACAね"
+              eng="Inside Joke"
+              lyric="ずっと真夜中でいいのに"
+              aragement="100回嘔吐, ZTMY"
+            />
+          }
+        />
+        <Route
+          path="/songs/aiturazenindoushoukai"
+          element={
+            <SongInfoPage
+              img="https://i.ytimg.com/vi/ZUwaudw8ht0/hq720.jpg"
+              name="あいつら全員同窓会"
+              vocal="ACAね"
+              eng="Inside Joke"
+              lyric="ずっと真夜中でいいのに"
+              aragement="100回嘔吐, ZTMY"
+            />
+          }
+        />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/mikushit.jsx
@@ -0,0 +1,7 @@
+//開発中
+
+function MikuShit(props) {
+  let mikucount= {};
+  console.log(mikucount)
+}
+export default MikuShit;
