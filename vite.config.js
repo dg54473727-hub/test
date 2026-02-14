2efd55be76ebc7f676cdf597869d6676bf2651e4
admin <dg54473727@gmail.com>
2026-02-14 20:32:02 +0800

    perbaikan

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,5 @@ import netlifyPlugin from "@netlify/vite-plugin-react-router";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), netlifyPlugin()],
-  base: "/test",
+  plugins: [react(), tailwindcss(), netlifyPlugin()]
 });
